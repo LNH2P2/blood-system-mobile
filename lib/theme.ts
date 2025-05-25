ef1c17e45fbedea1815113bb14a4d309d84a2e29
Le Thanh Long <longlt201203@gmail.com>
2025-05-25 20:15:09 +0700

    add color theme

---
+++ lib/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        primary: "#199A8E",
+        light: "#FFFFFF",
+        dark: "#101623",
+        lightGray: "#E8F3F1",
+        gray: "#ADADAD",
+        darkGray: "#7B7C7B",
+        danger: "#FF5C5C",
+        success: "#199A8E"
+    }
+}
