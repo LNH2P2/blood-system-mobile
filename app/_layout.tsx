908e0ddc39bf8346c72cbe6b1ad3a4057182a710
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-05-28 13:36:09 +0700

    feat: add layout route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Stack } from 'expo-router'
 
-const client = new QueryClient();
+const client = new QueryClient()
 
 export default function RootLayout() {
-    return (
-        <QueryClientProvider client={client}>
-            <Stack screenOptions={{ headerShown: false }} />
-        </QueryClientProvider>
-    )
+  return (
+    <QueryClientProvider client={client}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='index' />
+        <Stack.Screen name='(member)' />
+        <Stack.Screen name='(admin)' />
+      </Stack>
+    </QueryClientProvider>
+  )
 }
