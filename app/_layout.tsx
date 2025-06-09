770aa6ff3f920d3c0dc89f2e42f82474c0cdc94e
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-06-09 23:07:22 +0700

    fix(layout): standardize import formatting and add missing hospital screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,20 @@
-import { AlertProvider } from '@/lib/hooks/useAlert'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Stack } from 'expo-router'
+import { AlertProvider } from "@/lib/hooks/useAlert";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
 
-const client = new QueryClient()
+const client = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={client}>
       <AlertProvider>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='index' />
-          <Stack.Screen name='(tabs)' />
-          <Stack.Screen name='(donation-request)' />
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="(donation-request)" />
+          <Stack.Screen name="(hospital)" />
         </Stack>
       </AlertProvider>
     </QueryClientProvider>
-  )
+  );
 }
